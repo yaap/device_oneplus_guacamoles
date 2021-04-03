flawnn/device_oneplus_guacamole

guacamole: Add to lunch menu

AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/derp_guacamole.mk
+
+COMMON_LUNCH_CHOICES := \
+	derp_guacamole-user \
+	derp_guacamole-userdebug \
+	derp_guacamole-eng

yaap/device_oneplus_guacamoles

AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosip_guacamole.mk
+
+COMMON_LUNCH_CHOICES := \
+	aosip_guacamole-user \
+	aosip_guacamole-userdebug \
+	aosip_guacamole-eng